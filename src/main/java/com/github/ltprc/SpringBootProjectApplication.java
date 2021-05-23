1125ba74c3f3eb5ac4101bb83333507d525e7050
Tuo Li <lit8@miamioh.edu>
2021-05-23 23:41:41 +0800

    First demo

--- src/main/java/com/github/ltprc/SpringBootProjectApplication.java
+++ src/main/java/com/github/ltprc/SpringBootProjectApplication.java
@@ -2,12 +2,14 @@ package com.github.ltprc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.github.ltprc")
 public class SpringBootProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+    }
 
 }
